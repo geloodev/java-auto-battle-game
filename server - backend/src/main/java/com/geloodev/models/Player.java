cb3f078d124f4c9e83ad6b9c2e0efc5cf244a5f4
Ângelo Holandini <angeloholandini11@gmail.com>
2023-06-11 18:24:47 -0300

    :recycle: refactor: pull up fields id and name  + removing getters and setters

--- server - backend/src/main/java/com/geloodev/models/Player.java
+++ server - backend/src/main/java/com/geloodev/models/Player.java
@@ -5,51 +5,12 @@ import java.util.UUID;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Player")
-public class Player {
-
-    @Id
-    @Column(name = "player_id")
-    private UUID playerId;
-
-    @Column(name = "username")
-    private String username;
+@Table(name = "player")
+public class Player extends Base {
 
     @Column(name = "email")
     private String email;
 
     @Column(name = "password")
     private String password;
-
-    public UUID getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(UUID playerId) {
-        this.playerId = playerId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
