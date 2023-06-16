68a2ff9942745018f0b62dc0a80bd15dec4f4e6a
Ângelo Holandini <angeloholandini11@gmail.com>
2023-06-16 09:09:35 -0300

    :sparkles: feat: insert, update, delete and open functions

--- server - backend/src/main/java/com/geloodev/utils/HibernateUtil.java
+++ server - backend/src/main/java/com/geloodev/utils/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.geloodev.utils;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -15,7 +16,50 @@ public class HibernateUtil {
         }
     }
 
-    public static SessionFactory geSessionFactory() {
+    public static void insert(Object object) {
+        Session session = HibernateUtil.open();
+        try {
+            session.beginTransaction();
+            session.persist(object);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+            exception.getMessage();
+        } 
+        session.close();
+    }
+
+    public static void update(Object object) {
+        Session session = HibernateUtil.open();
+        try {
+            session.beginTransaction();
+            session.merge(object);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+            exception.getMessage();
+        } 
+        session.close();
+    }
+
+    public static void delete(Object object) {
+        Session session = HibernateUtil.open();
+        try {
+            session.beginTransaction();
+            session.remove(object);
+            session.getTransaction().commit();
+        } catch (Exception exception) {
+            session.getTransaction().rollback();
+            exception.getMessage();
+        } 
+        session.close();
+    }
+
+    public static Session open() {
+        return getSessionFactory().openSession();
+    }
+
+    public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 }
