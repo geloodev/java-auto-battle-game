03a74030f5fa9d37098a80b647d35b2c7229620c
Ângelo Holandini <angeloholandini11@gmail.com>
2023-06-16 09:07:01 -0300

    :sparkles: feat: getters and setters for base and player models

--- server - backend/src/main/java/com/geloodev/models/Base.java
+++ server - backend/src/main/java/com/geloodev/models/Base.java
@@ -8,9 +8,26 @@ import jakarta.persistence.*;
 public abstract class Base {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private UUID id;
 
     @Column(name = "name")
     private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- server - backend/src/main/java/com/geloodev/models/Player.java
+++ server - backend/src/main/java/com/geloodev/models/Player.java
@@ -1,6 +1,4 @@
-package com.geloodev.models;
-
-import java.util.UUID;
+package com.geloodev.models;    
 
 import jakarta.persistence.*;
 
@@ -13,4 +11,20 @@ public class Player extends Base {
 
     @Column(name = "password")
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
