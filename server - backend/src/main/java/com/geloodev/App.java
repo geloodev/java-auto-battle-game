ed28e879548492971d6e93e3495c3d0ed807b23f
Ângelo Holandini <angeloholandini11@gmail.com>
2023-07-03 08:52:17 -0300

    :recycle: refactor: grouping commands in InicializerFacade

--- server - backend/src/main/java/com/geloodev/App.java
+++ server - backend/src/main/java/com/geloodev/App.java
@@ -1,32 +1,9 @@
 package com.geloodev;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static spark.Spark.*;
-
-import com.geloodev.controllers.CharacterController;
-import com.geloodev.daos.CharacterDAO;
-import com.geloodev.daos.WeaponDAO;
-import com.geloodev.models.Character;
-import com.geloodev.models.Weapon;
-import com.geloodev.utils.FreeMarkerUtil;
-import com.geloodev.utils.HibernateUtil;
-
-import freemarker.template.Template;
-
 public class App {
     
-    public static void main(String[] args) throws IOException {
-        
-        FreeMarkerUtil.init();
-        HibernateUtil.init();
-
-        CharacterController characterController = new CharacterController(new Character(), new CharacterDAO());
-        characterController.list();
+    public static void main(String[] args) {
+       InitializerFacade.init();
     }
 
 }
